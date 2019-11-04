a4de66988ae8e5c8d0653866c57d95300fd99a8c
Victor Arowo <arowovici@gmail.com>
2019-11-04 14:30:12 +0100

    Refactored require to import/export with babel

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const express = require('express');
+import express from 'express';
 import accountRouter from './resources/account/accounts.router';
-
-const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/accounts', accountRouter);
 
-module.exports = server;
+export default server;
